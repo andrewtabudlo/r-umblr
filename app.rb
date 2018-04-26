3ca90ec8f604104cb933816927855b9a2f7afc8d
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-04-26 16:37:55 -0400

    db config routes

--- app.rb
+++ app.rb
@@ -2,15 +2,40 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'rake'
 require 'pg'
+require_relative './models/User.rb'
+require_relative './models/Post.rb'
+require_relative './models/Tag.rb'
+require_relative './models/Post_Tag.rb'
+require_relative './models/Comment.rb'
 
 set :database, {adapter: 'postgresql', database: 'rumblr'}
+
+configure do
+  enable :sessions unless test?
+  set :session_secret, "secret"
+end
 
 get '/' do
   erb :index
 end
 
-get '/signin' do
+get '/newpost' do
+  erb :newpost
+end
+
+get '/user/signin' do
   erb :signin, :layout => false
+end
+
+post '/user/signin' do 
+  @user = User.find_by(email: params[:email], password: params[:password])
+  if @user != nil
+    session[:id] = @user.id
+    erb :profile
+  else   
+  # Could not find this user. Redirecting them to the signup page
+    redirect '/signup'
+  end 
 end
 
 get '/signup' do
@@ -23,4 +48,9 @@ end
 
 get '/:user/posts' do
 
+end
+
+get '/logout' do
+  session.clear
+  redirect '/signin'
 end
